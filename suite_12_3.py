9eae9b9f856816ed3d38fe4575f9bf611d4a3469
Konstantin7553 <krasik150@gmail.com>
2024-10-15 15:37:19 +0300

    Add files via upload

---
+++ suite_12_3.py
@@ -0,0 +1,18 @@
+import unittest
+from tests_12_3 import RunnerTest, TournamentTest
+
+suite = unittest.TestSuite()
+suite.addTest(unittest.TestLoader().loadTestsFromTestCase(RunnerTest))
+suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TournamentTest))
+
+runner = unittest.TextTestRunner(verbosity=2)
+runner.run(suite)
+
+
+
+
+
+
+
+
+
